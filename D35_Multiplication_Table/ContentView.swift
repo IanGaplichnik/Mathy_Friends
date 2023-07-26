ee5aaa591bfd2c0e687581a36ac4837282ae016f
IanGaplichnik <ian.gaplichnik@protonmail.ch>
2023-07-26 08:13:35 +0100

    Initial Commit

---
+++ D35_Multiplication_Table/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  D35_Multiplication_Table
+//
+//  Created by MacBook on 26.7.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ D35_Multiplication_Table/D35_Multiplication_TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  D35_Multiplication_TableApp.swift
+//  D35_Multiplication_Table
+//
+//  Created by MacBook on 26.7.2023.
+//
+
+import SwiftUI
+
+@main
+struct D35_Multiplication_TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
